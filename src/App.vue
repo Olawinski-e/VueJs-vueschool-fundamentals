14d3bd67136e006114f117f388e32ad33e8aaf20
Estelle Mac <estelleolawinski@MacBook-Pro-de-Estelle-2.local>
2020-10-14 17:37:39 +0200

    computed properties

--- src/App.vue
+++ src/App.vue
@@ -15,22 +15,38 @@
     </div>
     <b-container fluid v-if="state === 'edit'">
       <b-row class="my-1">
-        <b-col sm="9">
+        <b-col sm="6">
           <b-form-input
             type="text"
             v-model="newItem"
-            placeholder="Add an object"
+            placeholder="Add an item"
             @keyup.enter="saveItem"
             class="mb-2"
           ></b-form-input>
         </b-col>
         <b-col sm="3">
-          <button @click="saveItem" class="btn btn-info">Save item</button>
+          <button
+            :disabled="newItem.length === 0"
+            @click="saveItem"
+            class="btn btn-info"
+          >
+            Save item
+          </button>
         </b-col>
       </b-row>
     </b-container>
     <ul>
-      <li v-for="(item, index) of items" :key="index">{{ item }}</li>
+      <li
+        v-for="(item, index) of reversedItem"
+        :key="index"
+        @click="togglePurchased(item)"
+        :class="[
+          item.purchased ? 'strikeout' : '',
+          item.highPriority ? 'priority' : '',
+        ]"
+      >
+        {{ item.label }}
+      </li>
     </ul>
     <p v-if="items.length === 0" class="">Nice job you've bought everything</p>
   </div>
@@ -46,29 +62,46 @@ export default {
       header: "Shopping list App",
       newItem: "",
       items: [
-        // "10 party hats", "2 board games", "20 cups"
+        {
+          label: "10 party hats",
+          purchased: false,
+          highPriority: true,
+        },
+        { label: "2 board games", purchased: true, highPriority: false },
+        { label: "20 cups", purchased: false, highPriority: false },
       ],
     };
   },
+  computed: {
+    reversedItem() {
+      return this.items.slice(0).reverse();
+    },
+  },
   methods: {
     saveItem() {
-      this.items.push(this.newItem);
+      this.items.push({
+        label: this.newItem,
+      });
       this.newItem = "";
     },
     changeState(newState) {
       this.state = newState;
+    },
+    togglePurchased(item) {
+      item.purchased = !item.purchased;
     },
   },
 };
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.strikeout {
+  text-decoration: line-through;
+}
+.priority {
+  color: orange;
+}
+li {
+  cursor: pointer;
 }
 </style>
